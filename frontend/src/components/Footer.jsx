3f04c3836ee7cf790740e02dfacae47254237c8f
Aaryan-Degama <aaryandegama@gmail.com>
2026-04-13 12:06:10 +0530

    Changes Made and Pls use a better branch name the branch name should the features name and not you ass Name 'Jalendu' and tmkc

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="flex justify-center items-center px-6 py-4 border-t border-[#2A2A2A] text-sm text-gray-400 gap-8">
+    <footer className="flex justify-center items-center px-6 py-4 border-t border-[#2A2A2A] text-sm text-gray-400 gap-8 text-[18px]">
       
       {/* Left Side - Copyright */}
-      <div>
-        &copy; {new Date().getFullYear()} Blastoise. All rights reserved
-      </div>
+      
 
       {/* Right Side - Links */}
       <div className="flex space-x-6">
+        <div>
+          &copy; {new Date().getFullYear()} &nbsp;&nbsp; Blastoise - All rights reserved
+        </div>
         <a href="#" className="hover:text-white transition-colors duration-200">
           Members
         </a>
         <a href="#" className="hover:text-white transition-colors duration-200">
-          aaryan ki mkc
+          Members
         </a>
         <a href="#" className="hover:text-white transition-colors duration-200">
           67
@@ -24,7 +25,7 @@ function Footer() {
           Terms of Service
         </a>
         <a href="#" className="hover:text-white transition-colors duration-200">
-          Contact
+          Contact Us
         </a>
       </div>
       
